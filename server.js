f5c9750b96c050bef1f259cf0aae73d293f69f65
Alek <56727628+aseawright1@users.noreply.github.com>
2020-03-31 17:51:39 -0500

    create main menu of app (?)
    getting a mySQL connection error...

---
+++ server.js
@@ -0,0 +1,39 @@
+// creating the connection
+const mysql = require('mysql')
+const inquirer = require("inquirer")
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3000,
+    user: 'root',
+    password: 'password',
+    database: 'employee-tracker.db'
+})
+connection.connect((err) => {
+    if (err) throw (err)
+    mainMenu()
+})
+
+// main menu and prompts screen
+function mainMenu() {
+    inquirer.prompt({
+        type: 'list',
+        message: 'Make a selection',
+        name: 'selection',
+        choices: ['View all employees', 'Add employee', 'Update employee role', 'View departments', 'Add department', 'View roles', 'Add role', "End session"]
+    })
+    .then( answer => {
+        switch (answer.selection) {
+            case 'View all employees': viewAllEmps(); break
+            case 'Add employee': addEmps(); break
+            case 'Update employee role': updateRoles(); break
+            case 'View departments': viewDepts(); break
+            case 'Add departments': addDepts(); break
+            case 'View roles': viewRoles(); break
+            case 'Add role': addRoles(); break
+            case 'End session': connection.end(); break
+        }
+    })
+}
+
+// prompts below
