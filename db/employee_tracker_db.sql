47a48b84e2d0a073534dee50e4c08c65152a0d13
Alek <56727628+aseawright1@users.noreply.github.com>
2020-04-02 14:30:23 -0500

    added 'add employee' functionality

--- db/employee_tracker_db.sql
+++ db/employee_tracker_db.sql
@@ -5,23 +5,23 @@ USE employee_tracker_db;
 
 CREATE TABLE department
 (
-  dept_id INT NOT NULL,
+  dept_id INT NOT NULL AUTO_INCREMENT,
   dept_name VARCHAR(25) NULL,
   PRIMARY KEY (dept_id)
 );
 
 CREATE TABLE role
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(25) NULL,
-  salary DECIMAL(7,2),
+  salary DECIMAL(10,2),
   dept_id INT,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employees
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(25) NULL,
   last_name VARCHAR(25) NULL,
   role_id INT NULL,
@@ -30,4 +30,5 @@ CREATE TABLE employees
 );
 
 
-SELECT * FROM employees;
+SELECT first_name AS "First Name", last_name AS "Last Name", role_id AS "Rople ID", mgr_id AS "Manager ID"
+FROM employees;
--- server.js
+++ server.js
@@ -22,18 +22,59 @@ function mainMenu() {
         name: 'selection',
         choices: ['View all employees', 'Add employee', 'Update employee role', 'View departments', 'Add department', 'View roles', 'Add role', "End session"]
     })
-    .then( answer => {
+    .then(answer => {
         switch (answer.selection) {
             case 'View all employees': viewAllEmps(); break
             case 'Add employee': addEmps(); break
-            case 'Update employee role': updateRoles(); break
             case 'View departments': viewDepts(); break
             case 'Add departments': addDepts(); break
             case 'View roles': viewRoles(); break
             case 'Add role': addRoles(); break
+            case 'Update employee role': updateRoles(); break
             case 'End session': connection.end(); break
         }
     })
 }
 
-// prompts below
+function viewAllEmps() {
+    connection.query('SELECT * FROM employees', function(err, res) {
+        if (err) throw err
+        console.table(res)
+        mainMenu()
+    })
+}
+function addEmps(first_name, last_name, role_id, mgr_id) {
+    inquirer.prompt([
+        { type: 'input', message: 'First name', name: 'newFirstName' },
+        { type: 'input', message: 'Last name', name: 'newLastName' },
+        { type: 'number', message: 'Enter employee\'s role ID', name: 'newEmpRole' },
+        { type: 'input', message: 'Enter manager name', name: 'newMgrID' }
+    ])
+    .then(answer => {
+        connection.query('INSERT INTO employees SET ?', {
+            first_name: answer.newFirstName,
+            last_name: answer.newLastName,
+            role_id: answer.newEmpRole,
+            mgr_id: answer.newMgrID
+        }, function (err, res) {
+            if (err) throw err
+            console.log(`${answer.newFirstName} ${answer.newLastName} added to database`)
+            mainMenu();
+        })
+    })
+}
+function viewDepts() {
+
+}
+function addDepts() {
+
+}
+function viewRoles() {
+
+}
+function addRoles() {
+    
+}
+function updateRoles() {
+    
+}
